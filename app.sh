6543ec31f8f0c31a32ea848224069788b974f5ae
Lynn <lynn80827@hotmail.com>
2018-02-12 15:49:27 +0800

    provison for demo-laravel-todo:rest-api

--- app.sh
+++ app.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get -y update
-sudo apt-get -y install curl nginx php7.0 php7.0-fpm php7.0-mysql php7.0-sqlite3 php7.0-mbstring php-xml php7.0-zip
+sudo apt-get -y install unzip curl nginx
+sudo apt-get -y install yarn nodejs
+sudo apt-get -y install php7.0 php7.0-fpm php7.0-mysql php7.0-sqlite3 php7.0-mbstring php-xml php7.0-zip
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-git clone https://github.com/lynn80827/demo-laravel-todo.git laravel
+wget https://github.com/lynn80827/demo-laravel-todo/archive/rest-api.zip
+unzip rest-api.zip
+mv demo-laravel-todo-rest-api laravel
 cd laravel
 composer install --quiet
 cat > .env <<EOF
@@ -13,6 +20,11 @@ APP_DEBUG=true
 APP_LOG_LEVEL=error
 APP_URL=http://localhost
 EOF
+cd f2e
+yarn install --pure-lockfile
+yarn run build
+cd ..
+cp -R f2e/build/* public
 sudo chown -R www-data:www-data /home/ubuntu/laravel
 sudo chmod -R 755 /home/ubuntu/laravel/storage
 sudo chmod -R 755 /home/ubuntu/laravel/public
@@ -20,17 +32,21 @@ sudo tee /etc/nginx/sites-available/default <<EOF
 server {
         listen 80 default_server;
         listen [::]:80 default_server;
+
         root /home/ubuntu/laravel/public;
-        index index.html index.htm index.nginx-debian.html index.php;
         server_name localhost;
 
-        location / {
+        location /api {
+                index index.php;
                 try_files \$uri \$uri/ /index.php?\$query_string;
         }
 
-        location ~ \.php\$ {
-                include snippets/fastcgi-php.conf;
+        location / {
+                index index.html;
+        }
 
+        location ~ \.php$ {
+                include snippets/fastcgi-php.conf;
                 fastcgi_pass unix:/run/php/php7.0-fpm.sock;
         }
 }
